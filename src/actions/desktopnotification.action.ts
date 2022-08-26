d714c49162be6da07899d0a4376e1acf88fe5595
Pei-Yao Hung <peiyaoh@gmail.com>
2022-08-26 17:09:59 -0400

    add desktop notification just for testing

---
+++ src/actions/desktopnotification.action.ts
@@ -0,0 +1,21 @@
+import notifier from "node-notifier";
+
+
+export async function createDesktopNotification(title:string, message:string) {
+    console.log(`createDesktopNotification: ${title}, ${message}`);
+
+    notifier.notify(
+      {
+        title: title,
+        message: message,
+        //icon: path.join(__dirname, 'coulson.jpg'), // Absolute path (doesn't work on balloons)
+        sound: false, // Only Notification Center or Windows Toasters
+        wait: true // Wait with callback, until user action is taken against notification
+      },
+      function(err, response) {
+        // Response is response from notification
+      }
+    );
+  }
+
+
---
+++ src/test_script/test_desktop_notification.ts
@@ -0,0 +1,3 @@
+import { createDesktopNotification } from "../actions/desktopnotification.action";
+
+createDesktopNotification("[Desktop]", "Hi Pei-Yao!!!");
