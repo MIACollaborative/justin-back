724be3fb8f0117b8b228773f12e4b8b97f86376f
Pei-Yao Hung <peiyaoh@gmail.com>
2022-10-17 17:55:54 -0400

    Events now will be moveto archive collection
     once processed  (delete + insert)

--- src/db/events.service.ts
+++ src/db/events.service.ts
@@ -5,6 +5,10 @@ import { TriggerRecord } from '../models/triggerrecord.model';
 
 let drCollection: Collection;
 
+let archiveCollection: Collection;
+
+
+
 export async function getEventCollection() {
     if (!drCollection) {
         const db = await getDB();
@@ -13,7 +17,40 @@ export async function getEventCollection() {
     return drCollection;
 }
 
+export async function getEventArchiveCollection() {
+    if (!archiveCollection) {
+        const db = await getDB();
+        archiveCollection = db.collection(process.env.EVENT_ARCHIVE_COLLECTION_NAME!);
+    }
+    return archiveCollection;
+}
+
 export async function addEvent(record: Object) {
     let uColl = await getEventCollection();
-    await uColl.insertOne(record);
+    return await uColl.insertOne(record);
+}
+
+export async function updateEvent(record: Object, update: Object) {
+    console.log(`updateEvent(): id: ${record["_id"]}`);
+    let uColl = await getEventCollection();
+    return await uColl.updateOne({_id: new ObjectId( record["_id"]) }, update);
+}
+
+export async function archiveEvent(record: Object, update: Object) {
+    console.log(`archiveEvent(): id: ${record["_id"]}`);
+    let uColl = await getEventCollection();
+
+    let aColl = await getEventArchiveCollection();
+
+    // first, insert to the new archive
+    let archiveItem = {...record, ...update, _id: undefined};
+    console.log(`About to insert into archive: ${JSON.stringify(archiveItem)}`);
+    let archivedRecord = await aColl.insertOne(archiveItem);
+
+    // second, remove it from the event queue
+    console.log(`About to remove from the original event queue`);
+    let deleteRecord = await uColl.deleteOne({_id: new ObjectId( record["_id"]) });
+    console.log(`deleteRecord: ${JSON.stringify(deleteRecord)}`);
+
+    return archivedRecord;
 }
--- src/models/clockevent.model.ts
+++ src/models/clockevent.model.ts
@@ -1,20 +1,23 @@
 import { User } from "./user.model";
 import { ITrigger } from './trigger.interface';
 import { GenericRecord } from "./genericrecord.model";
+import { GenericEvent } from "./genericevent.model";
 
-export class GenericEvent {
+export class ClockEvent extends GenericEvent {
+    // As a start, use genericevent 
+    // assuming that the providedTimestamp is the intended clock value
+    
+    /*
     name: string;
     userName: string;
-    status: string;
     providedTimestamp: Date;
     generatedTimestamp: Date;
-
 
     constructor(name: string, userName: string, timestamp: Date) {
         this.name = name;
         this.userName = userName;
         this.providedTimestamp = timestamp;
         this.generatedTimestamp = new Date();
-        this.status = "new";
     }
+    */
 }
